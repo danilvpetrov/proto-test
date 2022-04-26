71d7c28244bc40d7a9cf1f4a78459b400a2784de
Des Little <des.little@betsecure.com>
2022-04-26 14:09:55 +1000

    Implemented cooncurrent bilateral communication on the client.

--- cmd/ping/main.go
+++ cmd/ping/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"context"
-	"io"
 	"log"
 	"os"
+	"time"
 
 	"github.com/danilvpetrov/proto-test/data"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
 func main() {
@@ -31,25 +34,68 @@ func main() {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	stream, err := client.DoPingPong(ctx, &data.PingRequest{
-		Ping: &data.Ping{
-			Text: "Ping",
-		},
-	})
+	stream, err := client.DoPingPong(ctx)
 	if err != nil {
 		panic(err)
 	}
 
-	for {
-		resp, err := stream.Recv()
-		if err == io.EOF {
-			log.Print("stream closed")
-			return
-		}
-		if err != nil {
-			log.Fatalf("cannot receive %v", err)
-		}
-		log.Printf("resp received: %s", resp.Pong.Text)
-	}
+	eg, ctx := errgroup.WithContext(ctx)
 
+	eg.Go(func() error {
+		return recv(ctx, stream)
+	})
+
+	eg.Go(func() error {
+		return send(ctx, stream)
+	})
+
+	eg.Wait()
+
+}
+
+func recv(ctx context.Context, receiver interface {
+	Recv() (*data.PongResponse, error)
+}) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		resp, err := receiver.Recv()
+		if err != nil {
+			return err
+		}
+
+		if resp.Pong.Text != "Pong" {
+			return status.Error(codes.InvalidArgument, "Invalid argument in the response")
+		}
+
+		log.Printf("received a response %v", resp.Pong)
+	}
+}
+
+func send(ctx context.Context, sender interface {
+	Send(*data.PingRequest) error
+}) error {
+	t := time.NewTicker(2 * time.Second)
+	defer t.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return status.Error(codes.Aborted, ctx.Err().Error())
+		case <-t.C:
+			if err := sender.Send(
+				&data.PingRequest{
+					Ping: &data.Ping{
+						Text: "Ping",
+					},
+				},
+			); err != nil {
+				return status.Error(codes.Internal, err.Error())
+			}
+		}
+	}
 }

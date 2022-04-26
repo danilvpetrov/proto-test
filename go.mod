0ab57ef62c50637806931dd4562a2078b09e0c8a
Danil Petrov <danilpetrovvi@gmail.com>
2022-04-26 13:46:16 +1000

    Implement concurrent bilateral data transfer on the server.

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/danilvpetrov/proto-test
 go 1.18
 
 require (
+	golang.org/x/sync v0.0.0-20190423024810-112230192c58
 	google.golang.org/grpc v1.45.0
 	google.golang.org/protobuf v1.28.0
 )
--- pong/server.go
+++ pong/server.go
@@ -1,9 +1,12 @@
 package pong
 
 import (
+	"context"
+	"log"
 	"time"
 
 	"github.com/danilvpetrov/proto-test/data"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -16,22 +19,59 @@ type Server struct {
 // DoPingPong performs handling of the Ping request from the client and returns
 // Pong response.
 func (s *Server) DoPingPong(
-	req *data.PingRequest,
 	stream data.PingPong_DoPingPongServer,
 ) error {
-	if req.Ping.Text != "Ping" {
-		return status.Error(codes.InvalidArgument, "Invalid argument in the request")
-	}
+	ctx, cancel := context.WithCancel(stream.Context())
+	defer cancel()
 
+	eg, ctx := errgroup.WithContext(ctx)
+
+	eg.Go(func() error {
+		return recv(ctx, stream)
+	})
+
+	eg.Go(func() error {
+		return send(ctx, stream)
+	})
+
+	return eg.Wait()
+}
+
+func recv(ctx context.Context, receiver interface {
+	Recv() (*data.PingRequest, error)
+}) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
+
+		req, err := receiver.Recv()
+		if err != nil {
+			return err
+		}
+
+		if req.Ping.Text != "Ping" {
+			return status.Error(codes.InvalidArgument, "Invalid argument in the request")
+		}
+
+		log.Printf("received a request %v", req)
+	}
+}
+
+func send(ctx context.Context, sender interface {
+	Send(*data.PongResponse) error
+}) error {
 	t := time.NewTicker(2 * time.Second)
 	defer t.Stop()
 
 	for {
 		select {
-		case <-stream.Context().Done():
-			return status.Error(codes.Aborted, stream.Context().Err().Error())
+		case <-ctx.Done():
+			return status.Error(codes.Aborted, ctx.Err().Error())
 		case <-t.C:
-			if err := stream.Send(
+			if err := sender.Send(
 				&data.PongResponse{
 					Pong: &data.Pong{
 						Text: "Pong",
